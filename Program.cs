6626194b52f13d25a3bf6b8d85fc6eb03d77f83d
Buse Nur <ogunsenbusenur@hotmail.com>
2025-12-20 12:32:09 +0300

    Set Dashboard as default home page

--- Program.cs
+++ Program.cs
@@ -49,7 +49,8 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
 
 app.MapRazorPages();
 
